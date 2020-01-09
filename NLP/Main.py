835db4473f36fb99c666ab3747f4222ceff6ce49
Denis Neustroev <denis245n1@gmail.com>
2020-01-09 22:40:42 +0500

    delete_trash
    Delete my previous works

--- NLP/Main.py
+++
@@ -1,7 +0,0 @@
-# from NLP_with_Python import chapter_1
-# from NLP_with_Python import chapter_2
-from NLP_with_Python import chapter_3
-
-if __name__ == "__main__":
-    # chapter_1.call()
-    chapter_3.call()
